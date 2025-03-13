6e7c25d412a548d5dc629bc8d5c4f0b816d59f93
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2025-03-13 15:07:10 +0100

    [TASK] Improve FlashMessages ViewHelper page (#102)
    * [TASK] Improve FlashMessages ViewHelper page
    
    Releases: main, 13.4
    
    * Apply suggestions from code review
    
    * Update FlashMessages.rst
    
    ---------
    
    Co-authored-by: Stefan Frömken <123929835+sfroemkenjw@users.noreply.github.com>

---
+++ Documentation/Global/_FlashMessages/_MyController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class MyController extends ActionController
+{
+    public function demonstrateFlashMessagesAction(): ResponseInterface
+    {
+        $this->addFlashMessage('This is a success message.');
+        $this->addFlashMessage('This is a warning.', 'Warning Headline', ContextualFeedbackSeverity::WARNING);
+        return $this->htmlResponse();
+    }
+}
